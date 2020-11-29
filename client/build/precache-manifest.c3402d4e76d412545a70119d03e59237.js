185bcc095d679030547de8992dc268ef4634f327
hamzah <hamzah.umar@cs.ucalgary.ca>
2020-11-29 11:41:36 -0700

    Advanced Search - release build

---
+++ client/build/precache-manifest.c3402d4e76d412545a70119d03e59237.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "582f3e76fddc3fbf42f8409834210229",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85970619e5c7057ef462",
+    "url": "/static/css/3.da55fe1a.chunk.css"
+  },
+  {
+    "revision": "20f6cccdd217dfa5e849",
+    "url": "/static/css/main.8ced6600.chunk.css"
+  },
+  {
+    "revision": "476ca684d026d1fc45f6",
+    "url": "/static/js/2.79d46504.chunk.js"
+  },
+  {
+    "revision": "02e13e2a693d7a19ee10ccc313e869f9",
+    "url": "/static/js/2.79d46504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85970619e5c7057ef462",
+    "url": "/static/js/3.0e9a24ea.chunk.js"
+  },
+  {
+    "revision": "20f6cccdd217dfa5e849",
+    "url": "/static/js/main.59cf3088.chunk.js"
+  },
+  {
+    "revision": "859d7c4abc270eeeb567",
+    "url": "/static/js/runtime-main.da166ac6.js"
+  },
+  {
+    "revision": "a51f41502a4d265ebad4eec60f9a9cce",
+    "url": "/static/media/exportBackground.a51f4150.png"
+  },
+  {
+    "revision": "8f897ef02183d14b1416c18e732936f3",
+    "url": "/static/media/homescreen.8f897ef0.svg"
+  },
+  {
+    "revision": "c413c538c50a8b827d9dba1c82a8749e",
+    "url": "/static/media/mockups.c413c538.png"
+  }
+]);
