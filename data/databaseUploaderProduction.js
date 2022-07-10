b8f16e60c88b0c369c640bf8eb5e687528f2f873
Hamzah <37099415+HamzahX@users.noreply.github.com>
2022-07-10 11:18:00 -0600

    database uploaders optimization + minor re-factor

--- data/databaseUploaderProduction.js
+++ data/databaseUploaderProduction.js
@@ -1,114 +1,87 @@
 //initialize constants
-const path = require('path');
-const fs = require('fs');
 const mongoClient = require('mongodb').MongoClient;
 const mongoURI = `mongodb+srv://hamzah:${process.env.MONGOPASSWORD}@cluster0-wz8lb.mongodb.net/test?retryWrites=true&w=majority`;
 
 //globals
-var DB;
+let DB;
 
-var DEV_PLAYERS_COLLECTION;
-var DEV_COUNTRIES_COLLECTION;
-var DEV_STATS_REFERENCE_COLLECTION;
-var DEV_STATS_BY_POSITION_COLLECTION;
-var DEV_PERCENTILE_ARRAYS_COLLECTION;
-var DEV_CLUBS_COLLECTION;
+let DEV_PLAYERS_COLLECTION;
+let DEV_COUNTRIES_COLLECTION;
+let DEV_STATS_REFERENCE_COLLECTION;
+let DEV_STATS_BY_POSITION_COLLECTION;
+let DEV_PERCENTILE_ARRAYS_COLLECTION;
+let DEV_CLUBS_COLLECTION;
+let DEV_Z_SCORE_DATA_COLLECTION;
 
-var PROD_PLAYERS_COLLECTION;
-var PROD_COUNTRIES_COLLECTION;
-var PROD_STATS_REFERENCE_COLLECTION;
-var PROD_STATS_BY_POSITION_COLLECTION;
-var PROD_PERCENTILE_ARRAYS_COLLECTION;
-var PROD_CLUBS_COLLECTION;
+let PROD_PLAYERS_COLLECTION;
+let PROD_COUNTRIES_COLLECTION;
+let PROD_STATS_REFERENCE_COLLECTION;
+let PROD_STATS_BY_POSITION_COLLECTION;
+let PROD_PERCENTILE_ARRAYS_COLLECTION;
+let PROD_CLUBS_COLLECTION;
+let PROD_Z_SCORE_DATA_COLLECTION;
 
 let setup = async () => {
 
-    return new Promise(function(resolve, reject) {
+    console.time('database connection');
 
-        console.time('database connection');
+    const client = await mongoClient.connect(mongoURI, {useUnifiedTopology: true});
 
-        mongoClient.connect(mongoURI, {useUnifiedTopology: true},function (err, client) {
-            if (err) {
-                console.log(err);
-                reject();
-            }
+    DB = client.db("ProjectFourteen");
 
-            DB = client.db("ProjectFourteen");
+    DEV_CLUBS_COLLECTION = DB.collection("Clubs_Dev");
+    DEV_COUNTRIES_COLLECTION = DB.collection("Countries_Dev");
+    DEV_STATS_REFERENCE_COLLECTION = DB.collection("StatsReferenceData_Dev");
+    DEV_STATS_BY_POSITION_COLLECTION = DB.collection("StatsByPosition_Dev");
+    DEV_PLAYERS_COLLECTION = DB.collection("Players_Dev");
+    DEV_PERCENTILE_ARRAYS_COLLECTION = DB.collection("PercentileArrays_Dev");
+    DEV_Z_SCORE_DATA_COLLECTION = DB.collection("ZScoreData_Dev");
 
-            DEV_CLUBS_COLLECTION = DB.collection("Clubs_Dev");
-            DEV_COUNTRIES_COLLECTION = DB.collection("Countries_Dev");
-            DEV_STATS_REFERENCE_COLLECTION = DB.collection("StatsReferenceData_Dev");
-            DEV_STATS_BY_POSITION_COLLECTION = DB.collection("StatsByPosition_Dev");
-            DEV_PLAYERS_COLLECTION = DB.collection("Players_Dev");
-            DEV_PERCENTILE_ARRAYS_COLLECTION = DB.collection("PercentileArrays_Dev");
+    PROD_CLUBS_COLLECTION = DB.collection("Clubs");
+    PROD_COUNTRIES_COLLECTION = DB.collection("Countries");
+    PROD_STATS_REFERENCE_COLLECTION = DB.collection("StatsReferenceData");
+    PROD_STATS_BY_POSITION_COLLECTION = DB.collection("StatsByPosition");
+    PROD_PLAYERS_COLLECTION = DB.collection("Players");
+    PROD_PERCENTILE_ARRAYS_COLLECTION = DB.collection("PercentileArrays");
+    PROD_Z_SCORE_DATA_COLLECTION = DB.collection("ZScoreData")
 
-            PROD_CLUBS_COLLECTION = DB.collection("Clubs");
-            PROD_COUNTRIES_COLLECTION = DB.collection("Countries");
-            PROD_STATS_REFERENCE_COLLECTION = DB.collection("StatsReferenceData");
-            PROD_STATS_BY_POSITION_COLLECTION = DB.collection("StatsByPosition");
-            PROD_PLAYERS_COLLECTION = DB.collection("Players");
-            PROD_PERCENTILE_ARRAYS_COLLECTION = DB.collection("PercentileArrays");
-
-            console.timeEnd('database connection');
-            resolve();
-        })
-
-    });
+    console.timeEnd('database connection');
 
 };
 
 
 let copyDevToProduction = async (devCollection, prodCollection) => {
 
-    return new Promise(function (resolve, reject) {
+    await prodCollection.deleteMany({});
 
-        prodCollection.deleteMany({}).then(
-            () => {
-                devCollection.find({}).toArray(async function(err, docs) {
-                    let bulkInsertArray = [];
-                    for (let i=0; i<docs.length; i++){
-                        bulkInsertArray.push(docs[i]);
-                    }
-                    prodCollection.insertMany(bulkInsertArray, function(err, res) {
-                        if (err) {
-                            console.log(err);
-                        }
-                        resolve();
-                    });
-                });
-            }
-        );
-
-    });
+    const documents = await devCollection.find({}).toArray();
+    await prodCollection.insertMany(documents);
 
 };
 
+let main = async () => {
 
-console.time('production database uploading');
-setup()
-    .then(async () => {
-        await copyDevToProduction(DEV_CLUBS_COLLECTION, PROD_CLUBS_COLLECTION)
+    console.time('production database uploading');
+
+    await setup();
+
+    await copyDevToProduction(DEV_CLUBS_COLLECTION, PROD_CLUBS_COLLECTION);
+    await copyDevToProduction(DEV_COUNTRIES_COLLECTION, PROD_COUNTRIES_COLLECTION);
+    await copyDevToProduction(DEV_STATS_REFERENCE_COLLECTION, PROD_STATS_REFERENCE_COLLECTION);
+    await copyDevToProduction(DEV_STATS_BY_POSITION_COLLECTION, PROD_STATS_BY_POSITION_COLLECTION);
+    await copyDevToProduction(DEV_PLAYERS_COLLECTION, PROD_PLAYERS_COLLECTION);
+    await copyDevToProduction(DEV_PERCENTILE_ARRAYS_COLLECTION, PROD_PERCENTILE_ARRAYS_COLLECTION);
+    await copyDevToProduction(DEV_Z_SCORE_DATA_COLLECTION, PROD_Z_SCORE_DATA_COLLECTION);
+
+    console.timeEnd('production database uploading');
+
+};
+
+main()
+    .then(() => {
+        process.exit(0)
     })
-    .then(async () => {
-        await copyDevToProduction(DEV_COUNTRIES_COLLECTION, PROD_COUNTRIES_COLLECTION)
-    })
-    .then(async () => {
-        await copyDevToProduction(DEV_STATS_REFERENCE_COLLECTION, PROD_STATS_REFERENCE_COLLECTION)
-    })
-    .then(async () => {
-        await copyDevToProduction(DEV_STATS_BY_POSITION_COLLECTION, PROD_STATS_BY_POSITION_COLLECTION)
-    })
-    .then(async () => {
-        await copyDevToProduction(DEV_PLAYERS_COLLECTION, PROD_PLAYERS_COLLECTION)
-    })
-    .then(async () => {
-        await copyDevToProduction(DEV_PERCENTILE_ARRAYS_COLLECTION, PROD_PERCENTILE_ARRAYS_COLLECTION)
-    })
-    .then(async () => {
-        console.timeEnd('production database uploading');
-        process.exit(0);
-    })
-    .catch(async(anError) => {
+    .catch(async (anError) => {
         console.log(anError);
         process.exit(-1);
     });
